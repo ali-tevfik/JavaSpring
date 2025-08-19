c632c341af591e14041b22529919aaf99ea5f491
ali-tevfik <tevfikdoner@hotmail.com>
2025-08-20 01:53:04 +0200

    Created jwt token and added jwtService,jwtAuth,Appconfig.

---
+++ jwt/src/main/java/com/example/jwt/JwtService.java
@@ -0,0 +1,59 @@
+package com.example.jwt;
+
+import java.security.Key;
+import java.util.Date;
+
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+
+
+@Component
+public class JwtService {
+
+    private static final String SECRET_KEY = "ogXO8fxtSOF4H6JHsJdraLw9XJ53mSLRFYeTFR74N0Y=";
+    
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder()
+        .setSubject(userDetails.getUsername())
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60  * 60 * 2))
+        .signWith(getKey(),SignatureAlgorithm.HS256)
+        .compact();
+    }
+
+    public Key getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    public String getUserNameByToken(String token){
+        return exportToken(token, Claims::getSubject);
+    }
+
+
+    public boolean isTokenExpired(String token){
+        Date expiredTokenDate = exportToken(token, Claims::getExpiration);
+        return new Date().before(expiredTokenDate);
+    }
+
+    public <T> T exportToken(String token,Function<Claims,T> claimsFunc){
+        Claims claims = Jwts.parserBuilder()
+        .setSigningKey(getKey())
+        .build()
+        .parseClaimsJws(token).getBody();
+
+        return claimsFunc.apply(claims);
+    }
+
+
+}
---
+++ jwt/src/main/java/com/example/model/User.java
@@ -0,0 +1,41 @@
+package com.example.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "User")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "username")
+    private String username;
+    
+    @Column(name = "password")
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+}
